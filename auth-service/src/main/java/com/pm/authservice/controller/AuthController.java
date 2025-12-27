32be8da7ac73f33eb4c70a415c0e294838351120
yash <yashwable12@gmail.com>
2025-12-27 19:23:05 +0530

    added controller

---
+++ auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
@@ -0,0 +1,21 @@
+package com.pm.authservice.controller;
+
+import com.pm.authservice.dto.LoginRequestDTO;
+import com.pm.authservice.dto.LoginResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+    @Operation(summary = "Generate token on User login")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(
+            @RequestBody LoginRequestDTO loginRequestDTO){
+
+    }
+
+
+}
