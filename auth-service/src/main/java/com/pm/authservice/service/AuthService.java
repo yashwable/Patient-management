848eb299a07470ef776995d3015b6bc1b29c2f03
yash <yashwable12@gmail.com>
2026-01-01 00:24:44 +0530

    added repository package

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -8,9 +8,19 @@ import java.util.Optional;
 
 @Service
 public class AuthService {
+    private final UserService userService;
+
+    public AuthService(UserService userService){
+        this.userService = userService;
+    }
 
     public Optional<String> authenticate(LoginRequestDTO loginRequestDTO){
-        Optional<User> user = userService.findByEmail(loginRequestDTO.getEmail());
+        Optional<User> user = userService
+                .findByEmail(loginRequestDTO.getEmail())
+                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(), u.getPassword()))
+                .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
+
+        return token;
     }
 
 }
